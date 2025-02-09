90f446b19fbfa42beaa1c3d305dbf71eb4eec6ee
Sriza Sen <srizasen54554@gmail.com>
2025-02-09 10:17:58 +0530

    Create exercise.js

---
+++ JS/Basics/Strings/exercise.js
@@ -0,0 +1,28 @@
+const sampleData = `This is a really long string made up of many characters`;
+
+// Write code to...
+
+// Create a new variable and set that value equal to sample data converted to all uppercase letters
+let upperCaseSampleData = sampleData.toUpperCase()
+// Create a new variable and set that value equal to sample data converted to all lowercase letters
+let lowerCaseSampleData = sampleData.toLowerCase()
+// Create a new variable and set that value equal to the concatenation of the following three variables
+const noun = "I "
+const verb = "jumped"
+const ending = 'ed'
+const concatenated = noun + verb + ending
+
+// Create a new variable indexOfA and set it equal to the numerical index of the first 'a' in the following string:
+const whereIsTheA = "I love to see halograms at concerts"
+const indexOfA = whereIsTheA.indexOf('a')
+console.log(indexOfA)
+
+// Store in a new variable nameLength the length of the following variable:
+const famousName = "Lincoln"
+const nameLength = famousName.length
+// NOTE:
+// Remember to use console.log to check your work and validate your code 
+console.log(nameLength)
+console.log(upperCaseSampleData)
+console.log(lowerCaseSampleData)
+console.log(concatenated)
